c6a5b5f2a1a12ea82284b666f6abbef713d504cf
Daniel Clark <danielrclark1977@gmail.com>
2020-03-11 15:37:08 -0500

    Add project files.

---
+++ PurchaseRewards/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PurchaseRewards
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
